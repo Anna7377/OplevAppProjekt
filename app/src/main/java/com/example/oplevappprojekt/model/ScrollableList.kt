54861f8f4d6b38ec0d35d7862e18aba98715df3b
anna7377 <s213370@dtu.dk>
2022-11-04 10:33:33 +0100

    initial

---
+++ app/src/main/java/com/example/oplevappprojekt/model/ScrollableList.kt
@@ -0,0 +1,6 @@
+package com.example.scrollablelistofbuttons.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class ScrollableList(@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int)
