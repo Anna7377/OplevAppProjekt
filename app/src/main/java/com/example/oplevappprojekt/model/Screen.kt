5fc255d4729d20dd3e27a86421952c64121e55ad
mina0130 <91070698+mina0130@users.noreply.github.com>
2022-11-22 12:42:39 +0100

    ideas initiated

--- app/src/main/java/com/example/oplevappprojekt/model/Screen.kt
+++
@@ -1,6 +0,0 @@
-package com.example.oplevappprojekt.model
-
-sealed class Screen(val route: String){
-    object CreateTrip : Screen(route = "create_trip")
-    object Trips : Screen(route = "trips_")
-}
